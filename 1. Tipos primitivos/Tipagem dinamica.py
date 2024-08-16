97444e7aad353e77cece31fa680a20b118ef8898
LeeoBHH <leonardomiranda1307@gmail.com>
2024-08-16 18:15:30 +0000

    primeiras aulas

---
+++ 1. Tipos primitivos/Tipagem dinamica.py
@@ -0,0 +1,12 @@
+import os
+
+os.system("cls || clear") # Limpa o terminal
+
+
+nome = "LeeoBHH"
+idade = 17
+altura = 1.73
+
+print(f"Nome: {nome}")
+print(f"idade: {idade}")
+print(f"Altura: {altura}")
---
+++ 1. Tipos primitivos/Tipagem estatica.py
@@ -0,0 +1,17 @@
+import os
+
+os.system("cls || clear") # Limpa o terminal
+#Tipagem estática
+
+
+nome : str # Equivalente a Cadeia
+idade : int # Equivalente a inteiro
+altura : float # Equivalente a Real
+
+nome = "Marta"
+idade = 20
+altura = 1.80
+
+print(f"Nome: {nome}") 
+print(f"Idade: {idade}")
+print(f"Altura: {altura}")   
