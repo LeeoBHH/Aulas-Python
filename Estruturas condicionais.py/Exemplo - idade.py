c43d5cd79f966b9445902920cb4a16e0700be2ff
LeeoBHH <leonardomiranda1307@gmail.com>
2024-08-20 23:39:33 +0000

    Python

---
+++ Estruturas condicionais.py/Exemplo - idade.py
@@ -0,0 +1,16 @@
+import os
+os.system("Clear || clear")
+
+# Solicitando dados.
+idade = int(input("Digite sua idade: "))
+
+
+# Verificando.
+# True é igual a verdadeiro.
+# False é iguaç a falso.
+if idade < 18:
+    print("Menoridade.")
+else:
+    print("Maioridade.")
+
+print("=== FIM ===")    
