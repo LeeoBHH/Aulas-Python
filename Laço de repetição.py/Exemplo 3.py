713a84155c40ed6bc73b232921ff9c222061c0c3
LeeoBHH <leonardomiranda1307@gmail.com>
2024-08-26 17:03:09 +0000

    Python

---
+++ Laço de repetição.py/Exemplo 3.py
@@ -0,0 +1,10 @@
+import os
+import time
+os.system("cls || clear")
+
+print("Laço da repetição - For")
+for i in range(1,90, 2): 
+        print(f"Conteúdo da variável i = {i}")
+        time.sleep(2) # Vai esperar 2 segundos.
+        
+print("=== FIM ===")
